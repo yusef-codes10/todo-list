77397f634345b00723c7e8d0dc9178450c0ce263
Youcef <youcefcopy@gmail.com>
2026-02-07 14:54:43 +0100

    the reactive variable are so REACTIVE

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
 import { ref } from 'vue'
 
 const todo = ref('Jake')
+
+const check = () => {}
 </script>
 
 <template>
   <div class="todo-container">
     <div class="user-input">
       <input type="text" v-model="todo" />
-      <button>Add</button>
+      <button @click="check">Add</button>
+    </div>
+    <div class="greet-user">
+      <h1>{{ todo }}</h1>
     </div>
   </div>
 </template>
