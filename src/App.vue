9f41f93b871bbd9e6ec798c9e30dde63985126e0
Youcef <youcefcopy@gmail.com>
2026-02-07 15:00:07 +0100

    dynamic update | greeting the user

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 
-const todo = ref('Jake')
+const todo = ref('')
+const inputTodo = ref('')
 
-const check = () => {}
+const check = () => {
+  todo.value = `hello ${inputTodo.value}`
+}
 </script>
 
 <template>
   <div class="todo-container">
     <div class="user-input">
-      <input type="text" v-model="todo" />
+      <input type="text" v-model="inputTodo" />
       <button @click="check">Add</button>
     </div>
     <div class="greet-user">
