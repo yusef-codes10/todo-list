d9d64c8ddab0056b61a556304dcd62aab8727aeb
Youcef <youcefcopy@gmail.com>
2026-02-07 14:51:09 +0100

    using the v-model to sync the UI & DOM of the input field

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const todo = ref('Jake')
+</script>
 
 <template>
   <div class="todo-container">
     <div class="user-input">
-      <input type="text" />
+      <input type="text" v-model="todo" />
       <button>Add</button>
     </div>
   </div>
