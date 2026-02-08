75414a0c1ea8fb0aef085e9bab48a7131db3fcbc
Youcef <youcefcopy@gmail.com>
2026-02-08 17:34:13 +0100

    adding setTimout to wait a lilttle bit for the fetching

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -27,17 +27,19 @@ const checkTodo = (index) => {
   todos.value[index].isDone = !todos.value[index].isDone
 }
 
-onMounted(async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const data = await response.json()
-    todos.value = data.map((todo) => ({
-      text: todo.title,
-      isDone: todo.completed,
-    }))
-  } catch (error) {
-    console.log(error)
-  }
+onMounted(() => {
+  setTimeout(async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      const data = await response.json()
+      todos.value = data.map((todo) => ({
+        text: todo.title,
+        isDone: todo.completed,
+      }))
+    } catch (error) {
+      console.log(error)
+    }
+  }, 4000)
 })
 </script>
 
